c097e231fdf76b7638d7070c10fcc6dccc6d064b
N-hiromi <ryupi-20@outlook.jp>
2023-01-26 11:42:37 +0000

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+    [
+      {
+        name: 'ジョーズ',
+        year: '2023',
+        description: '映画だよ',
+        image_url: 'https://picsum.photos/200',
+        is_showing: true,
+
+      },
+      {
+        name: 'ミルク',
+        year: '2022',
+        description: '映画だよ',
+        image_url: 'https://picsum.photos/200',
+        is_showing: false,
+      }
+    ]
+  )
