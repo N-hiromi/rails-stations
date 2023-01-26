9282d2fb51c70aaabdad87ea86a5a063864708e7
N-hiromi <ryupi-20@outlook.jp>
2023-01-26 23:13:15 +0000

    station3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'movies/', to: "movies#index"
+    resources :movies, only: [:index, :new, :create]
+    get '/movies/', to: "movies#index"
   end
   get "/movies", to: "movies#index"
 end
