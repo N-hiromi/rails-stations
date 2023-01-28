bdbb5d1c4e8dc693e98a06111e1801db7721a024
N-hiromi <ryupi-20@outlook.jp>
2023-01-28 08:44:49 +0000

    station6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,29 @@
 class MoviesController < ApplicationController
-	def index
-		@q = Movie.ransack(params[:q])
-		@movies = @q.result(distinct: true).order("created_at desc")
+	def index		
+		# @q = Movie.ransack(params[:q])
+		# @rangeは全てか上映中か上映予定か絞ったMovie配列
+		# @range = @q.result(distinct: true).order("created_at desc")
+		@rgp = params[:is_showing]
+		@keyp = params[:keyword]
+		
+		#入力値がなければ全て表示(初期表示含む)
+		if @rgp == "" && @keyp == ""
+			@movies = Movie.all
+		end
+
+		# @rangeは全てか上映中か上映予定か絞ったMovie配列
+		if @rgp == "1"
+			@range = Movie.where(is_showing: true)
+		elsif @rgp == "0"
+			@range = Movie.where(is_showing: false)
+		else
+			@range = Movie.all
+		end
+
+		if (@keyp == "")
+			@movies = @range
+		else
+			@movies = @range.where( "name LIKE ? OR description LIKE ? ", "%#{@keyp}%", "%#{@keyp}%")
+		end
 	end
 end
