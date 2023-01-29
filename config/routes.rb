a3514f7b33b792d0b771ca375d3da6b516813dc3
N-hiromi <ryupi-20@outlook.jp>
2023-01-29 04:39:23 +0000

    station9

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :movies, only: [:create, :update, :destroy]
     get '/movies/', to: "movies#index"
-    get '/movies/:id/edit', to: "movies#edit"
+    get '/movies/:id/', to: "movies#edit"
+    resources :schedules, only: [:create, :update, :destroy]
+    get '/schedules/', to: "schedules#index"
+    get '/schedules/:id/', to: "schedules#edit"
+    get '/movies/:id/schedules/new', to: "schedules#new"
   end
   get "/movies", to: "movies#index"
   resources :movies, only: [:show, :update, :destroy]
