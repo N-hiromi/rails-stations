cb7be85fca62fa9b06ca06451ebf3e6324bc9c1d
N-hiromi <ryupi-20@outlook.jp>
2023-01-30 01:21:12 +0000

    途中確認

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,9 +3,13 @@ class ReservationsController < ApplicationController
 		@reservation = Reservation.new
 		date = params[:date]
 		sheet_id = params[:sheet_id]
+		schedule_id = params[:schedule_id]
+		movie_id = Schedule.find(schedule_id).movie_id
+
 		if date == nil && sheet == nil
-			render status:302
-			redirect_to :reservation_movie_path
+			# render status:302
+			# redirect_to :reservation_movie_path
+			redirect_to("/movies/#{movie_id}/reservation")
 		end
 	end
 
@@ -20,8 +24,7 @@ class ReservationsController < ApplicationController
 				
 		# 同じ日付で同じ映画、同じ席が取られていないか確認
 		dup = Reservation.where( "schedule_id LIKE ? OR sheet_id LIKE ? ", "#{schedule_id}", "%#{sheet_id}%")
-
-		if dup != nil
+		if dup = nil
 			if @reservation.save
 				flash[:success] = "予約が完了しました"
 				redirect_to controller: :movies, action: :show, id: movie_id
@@ -30,9 +33,10 @@ class ReservationsController < ApplicationController
 				redirect_to action: :new, schedule_id: schedule_id, movie_id: movie_id, sheet_id: sheet_id, date: date
 			end
 		else
-			render status:302
+			# render {"status":302}
 			flash[:danger] = "その座席はすでに予約済みです"
-			redirect_to :reservation_movie_path, schedule_id: schedule_id, date: date
+			# redirect_to :movie_reservation_path, schedule_id: schedule_id, date: date
+			redirect_to("/movies/#{movie_id}/reservation?schedule_id=#{schedule_id}&date=#{date}")
 		end
 
   end
