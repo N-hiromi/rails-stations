299735b50f314c6eb6446e35cf16c7b332c09902
N-hiromi <ryupi-20@outlook.jp>
2023-01-30 00:42:00 +0000

    途中確認

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,29 +1,44 @@
 class ReservationsController < ApplicationController
 	def new
 		@reservation = Reservation.new
+		date = params[:date]
+		sheet_id = params[:sheet_id]
+		if date == nil && sheet == nil
+			render status:302
+			redirect_to :reservation_movie_path
+		end
 	end
 
 	def create
     @reservation = Reservation.new(reservation_params)
-    if @reservation.save
-      flash[:success] = "予約が完了しました"
-      # redirect_to controller: :movies, action: :index
-			schedule_id = reservation_params[:schedule_id]
-			movie_id = Schedule.find(schedule_id).movie_id
-			redirect_to controller: :movies, action: :show, id: movie_id
-    else
-      flash[:danger] = "登録失敗"
-			schedule_id = reservation_params[:schedule_id]
-			movie_id = Schedule.find(schedule_id).movie_id
-			sheet_id = reservation_params[:sheet_id]
-			date = reservation_params[:date]
-      # render action: :new, :locals => { :schedule_id => schedule_id, :movie_id => movie_id, :sheet_id => sheet_id, :date => date}
-			# render action: :new,  locals: { schedule_id: schedule_id, movie_id: movie_id, sheet_id: sheet_id, date: date}
-			redirect_to action: :new, schedule_id: schedule_id, movie_id: movie_id, sheet_id: sheet_id, date: date
-    end
+
+		# パラメータ
+		schedule_id = reservation_params[:schedule_id]
+		movie_id = Schedule.find(schedule_id).movie_id
+		sheet_id = reservation_params[:sheet_id]
+		date = reservation_params[:date]
+				
+		# 同じ日付で同じ映画、同じ席が取られていないか確認
+		dup = Reservation.where( "schedule_id LIKE ? OR sheet_id LIKE ? ", "#{schedule_id}", "%#{sheet_id}%")
+
+		if dup != nil
+			if @reservation.save
+				flash[:success] = "予約が完了しました"
+				redirect_to controller: :movies, action: :show, id: movie_id
+			else
+				flash[:danger] = "登録失敗"
+				redirect_to action: :new, schedule_id: schedule_id, movie_id: movie_id, sheet_id: sheet_id, date: date
+			end
+		else
+			render status:302
+			flash[:danger] = "その座席はすでに予約済みです"
+			redirect_to :reservation_movie_path, schedule_id: schedule_id, date: date
+		end
+
   end
 
 	def reservation_params
     params.require(:reservation).permit(:schedule_id, :sheet_id, :date, :name, :email)
   end
 end
+
